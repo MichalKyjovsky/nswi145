34cb2960ecf749d02553900fc6f62ffe57183507
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2022-03-23 00:10:09 +0100

    HW5 Init

---
+++ 05-cxf/src/main/java/example/HelloWorldClient.java
@@ -0,0 +1,10 @@
+package example;
+
+
+public class HelloWorldClient {
+    public static void main(String[] argv) {
+//        org.apache.cxf.ws.security.sts.provider.SecurityTokenService service = new NotificationProducerService().getPort();
+//        //invoke business method
+//        service.keyExchangeToken();
+    }
+}
---
+++ 05-cxf/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CXF - Client </title>
+</head>
+<body>
+<h1><%= "CXF Client!" %>
+</h1>
+<br/>
+<a href="cxfClient">CSF Client</a>
+</body>
+</html>
